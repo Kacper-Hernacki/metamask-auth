f65254858aefd959e1e1aee28ce27cd7f30e04f3
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2022-11-07 21:38:12 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -3,17 +3,51 @@ import styled from "styled-components";
 import MetamaskLogo from "./assets/Metamask-logo.png";
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState("");
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* MetaMask is installed */
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
   return (
     <Container>
       <div>
-        <h1>Click to connect wallet with dApp</h1>
-        <SelectBox>
-          <img src={MetamaskLogo} />
-        </SelectBox>
-        <p>
-          This dApp is a test platform created for learning purposes, do not
-          make transactions or connect to Mainnet
-        </p>
+        {walletAddress && walletAddress.length > 0 ? (
+          <WalletDetails>
+            <h1>
+              Connected:
+              <p>
+                {walletAddress.substring(0, 6)}...
+                {walletAddress.substring(38)}
+              </p>
+            </h1>
+          </WalletDetails>
+        ) : (
+          <>
+            <h1>Click to connect wallet with dApp</h1>
+            <SelectBox onClick={connectWallet}>
+              <img src={MetamaskLogo} />
+            </SelectBox>{" "}
+            <p>
+              This dApp is a test platform created for learning purposes, do not
+              make transactions or connect to Mainnet
+            </p>
+          </>
+        )}
       </div>
     </Container>
   );
@@ -51,4 +85,14 @@ const SelectBox = styled.div`
   padding: 30px;
   background-color: white;
   cursor: pointer;
+`;
+
+const WalletDetails = styled.span`
+  color: white;
+  display: flex;
+  p {
+    font-weight: bold;
+    margin-left: 6px;
+    font-size: 32px;
+  }
 `;
