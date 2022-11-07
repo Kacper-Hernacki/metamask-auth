246013f264d15156075bb4140e25b93742bf301b
Kacper Hernacki <kacperhernacki@Kacpers-MacBook-Pro.local>
2022-11-07 20:56:06 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import MetamaskLogo from "./assets/Metamask-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <div>
+        <h1>Click to connect wallet with dApp</h1>
+        <SelectBox>
+          <img src={MetamaskLogo} />
+        </SelectBox>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This dApp is a test platform created for learning purposes, do not
+          make transactions or connect to Mainnet
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #00001a;
+  display: grid;
+  place-items: center;
+
+  h1 {
+    color: white;
+    font-size: 72px;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 92px;
+  }
+
+  p {
+    color: white;
+    margin-top: 32px;
+    font-size: 24px;
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+const SelectBox = styled.div`
+  border: 3px solid white;
+  border-radius: 36px;
+  padding: 30px;
+  background-color: white;
+  cursor: pointer;
+`;
